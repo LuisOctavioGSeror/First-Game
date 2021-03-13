bc5d1953ebb46ec9bd3f7fa0f4ed76a86ae56f67
LuisOctavioGSeror <luisoctaviogalessoseror@protonmail.com>
2021-03-13 19:10:29 -0400

    game folder

---
+++ primeiroGame/src/com/empresa/entities/Enemy.java
@@ -0,0 +1,15 @@
+package com.empresa.entities;
+
+import java.awt.image.BufferedImage;
+
+public class Enemy extends Entity {
+
+	public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
+		super(x, y, width, height, sprite);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+}
---
+++ primeiroGame/src/com/empresa/world/World.java
@@ -0,0 +1,96 @@
+package com.empresa.world;
+
+import java.awt.Graphics;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.empresa.entities.*;
+
+import com.empresa.main.Game;
+
+public class World {
+	
+	private Tile[] tiles; 
+	public static int WIDTH, HEIGHT;
+	
+	public World(String path) {
+		
+		try {
+			BufferedImage map = ImageIO.read(getClass().getResource(path));
+			int[] pixels = new int[map.getWidth()*map.getHeight()];
+			WIDTH = map.getWidth();
+			HEIGHT = map.getHeight();
+			tiles = new Tile[map.getWidth()*map.getHeight()]; 
+			map.getRGB(0, 0, map.getWidth(), map.getHeight(), pixels, 0, map.getWidth());
+			for(int xx = 0; xx < map.getWidth(); xx++) {
+				for(int yy = 0 ; yy < map.getHeight(); yy++) {
+					int pixelAtual = pixels[xx + (yy*map.getWidth())];
+					tiles[xx + (yy*WIDTH)] = new FloorTile(xx*16, yy*16, Tile.TILE_FLOOR);
+					if(pixelAtual == 0xFF000000) {
+						//floor
+						tiles[xx + (yy*WIDTH)] = new FloorTile(xx*16, yy*16, Tile.TILE_FLOOR);
+					}
+					else if(pixelAtual == 0xFFFFFFFF) {
+						//wall  
+						tiles[xx + (yy*WIDTH)] = new FloorTile(xx*16, yy*16, Tile.TILE_WALL);
+
+				    }
+					else if(pixelAtual == 0xFF57007F) {
+						//player
+						Game.player.setX(xx*16);
+						Game.player.setY(yy*16);
+					}
+					else if(pixelAtual == 0xFF007F0E) {
+						//Enemy
+						Game.entities.add(new Enemy(xx*16, yy*16, 16, 16, Entity.ENEMY_EN));
+					}
+					else if(pixelAtual == 0xFFFF7200) {
+						//Weapon
+						Game.entities.add(new Weapon(xx*16, yy*16, 16, 16, Entity.WEAPON_EN));
+
+					}
+					else if(pixelAtual == 0xFF1CFF41) {
+						//LifePack
+						Game.entities.add(new LifePack(xx*16, yy*16, 16, 16, Entity.LIFEPACK_EN));
+
+					}
+					else if(pixelAtual == 0xFFFFD800) {
+						//Bullet
+						Game.entities.add(new Bullet(xx*16, yy*16, 16, 16, Entity.BULLET_EN));
+
+					}
+					
+					
+					
+			    }
+			}	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void render(Graphics g) {
+		int xstart = Camera.x >> 4;
+		int ystart = Camera.y >> 4;
+		
+		int xfinal = xstart + (Game.WIDTH >> 4);
+		int yfinal = ystart + (Game.HEIGHT >> 4);
+
+		for(int xx = xstart; xx <= xfinal; xx++) {
+			for(int yy = ystart; yy <= yfinal; yy++) {
+				
+				if(xx < 0 || yy < 0 || xx >= WIDTH || yy >= HEIGHT)
+					continue;
+				
+				Tile tile = tiles[xx + (yy*WIDTH)];
+				tile.render(g);
+			}
+		}
+	}
+	
+}
